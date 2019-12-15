c8b841da788dd269718fcbe3aaa78402da988da5
Eduard Hildebrandt <eduard.hildebrandt@code-match.io>
2019-12-15 23:45:09 +0100

    replaced ThreadLocal with BillPughSingleton

--- src/main/java/com/example/validation/ValidatorHolder.java
+++ src/main/java/com/example/validation/ValidatorHolder.java
@@ -5,15 +5,12 @@ import javax.validation.Validator;
 
 public class ValidatorHolder {
 
-	private static final ThreadLocal<Validator> VALIDATOR_THREAD_LOCAL = new ThreadLocal<>();
+	private static class BillPughSingleton {  // inner class to provide instance of class
+		private static final Validator VALIDATOR_THREAD_LOCAL = Validation.buildDefaultValidatorFactory().getValidator();
+	}
 
 	public static Validator getValidator() {
-		Validator validator = VALIDATOR_THREAD_LOCAL.get();
-		if (validator == null) {
-			validator = Validation.buildDefaultValidatorFactory().getValidator();
-			VALIDATOR_THREAD_LOCAL.set(validator);
-		}
-		return validator;
+		return BillPughSingleton.VALIDATOR_THREAD_LOCAL;
 	}
 
 }
