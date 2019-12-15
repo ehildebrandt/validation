a7b6f3ad4c92d6c9b0bdd1cf542e527ce36817c9
Eduard Hildebrandt <eduard.hildebrandt@code-match.io>
2019-12-15 23:42:08 +0100

    removed BillPughSingleton

--- src/main/java/com/example/validation/ValidatorHolder.java
+++ src/main/java/com/example/validation/ValidatorHolder.java
@@ -5,15 +5,13 @@ import javax.validation.Validator;
 
 public class ValidatorHolder {
 
-	private static class BillPughSingleton {  // Inner class to provide instance of class
-		private static final ThreadLocal<Validator> VALIDATOR_THREAD_LOCAL = new ThreadLocal<>();
-	}
+	private static final ThreadLocal<Validator> VALIDATOR_THREAD_LOCAL = new ThreadLocal<>();
 
 	public static Validator getValidator() {
-		Validator validator = BillPughSingleton.VALIDATOR_THREAD_LOCAL.get();
+		Validator validator = VALIDATOR_THREAD_LOCAL.get();
 		if (validator == null) {
 			validator = Validation.buildDefaultValidatorFactory().getValidator();
-			BillPughSingleton.VALIDATOR_THREAD_LOCAL.set(validator);
+			VALIDATOR_THREAD_LOCAL.set(validator);
 		}
 		return validator;
 	}
